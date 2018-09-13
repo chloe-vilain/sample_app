beb065f355dca5f7c192b276bb797d8aee6c3bcb
cwvilain <cwvilain@gmail.com>
2018-09-13 13:23:04 -0400

    Add Contact pages
    Add contact.html.ern and update test, routes, static_pages_controller
    to support page.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -15,7 +15,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_home_url
     assert_response :success
     assert_select "title", "#{@base_title}"
-    
+
   end
 
   test "should get help" do
@@ -28,6 +28,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
