cbbdc8388b9d6e3aeea456449ab16f22ae99b8b1
cwvilain <cwvilain@gmail.com>
2018-09-20 10:53:22 -0400

    Add Remember Me checkbox to make persistent session optional
    Add HTML for new checkbox in sessions/new.html.erb and add logic to
    persist or forget based on checkbox status in sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,16 @@ class SessionsController < ApplicationController
   #Finds the user by the email entered in the form.
   #Checks if the user is valid and if the password entered matches the password
   #hash (password digest) using built-in has_secure_password authenticate. if
-  #true, creates a temporary session using log_in, creates a persistant session
-  #using remember, and redirects to the user's profile page.
+  #true, creates a temporary session using log_in. If remember me is checked,
+  # creates a persistant session using remember. Redirects to the user's
+  #profile page.
   #If fails, show error message and re-render the page.
-  
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/ password combination'
