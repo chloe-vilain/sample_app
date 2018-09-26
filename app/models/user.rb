37c8a993a1a56d36413c866c64c7f86315560939
cwvilain <cwvilain@gmail.com>
2018-09-26 19:20:28 -0400

    Add micropost model
    Generate db migrate to add Micropost table with content (text) and
    user (relation) plus automatic date-time messages.
    
    Formalize user/ micropost relationship by adding has_many :microposts
    relation to User with :destroy dependancy. Also add belongs_to :user
    to Micropost model.
    
    Add basic validation to micropost model to ensure non-nil, <140
    chars, and user ID included. Also set default order to created desc.
    
    Add Micropost model test for validation and that the newest is returned
    first. Add User test to ensure microposts are destroyed when user is
    destroyed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   #Generates getter and setter functions for an attribute
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
