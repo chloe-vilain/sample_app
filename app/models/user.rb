db80d61b7ae81a235164acff3cd6dd934c6e5249
cwvilain <cwvilain@gmail.com>
2018-09-26 13:49:44 -0400

    Changed local setting instead to update attribute in DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -68,7 +68,7 @@ class User < ApplicationRecord
 
     def create_activation_digest
       self.activation_token = User.new_token
-      self.activation_digest = User.digest(activation_token)
+      update_attribute(:activation_digest, User.digest(activation_token)
       #create  the token and the digest
     end
 
